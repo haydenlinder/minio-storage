d2107ddca0c9a76acdd796e0ce94964431185730
haydenlinder <haydenlinder@gmail.com>
2022-06-23 16:42:53 -0700

    ENV

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /minio
 wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
-export MINIO_ACCESS_KEY="$ACCESS_KEY"
-export MINIO_SECRET_KEY="$SECRET_KEY"
+export MINIO_ROOT_PASSWORD="$MINIO_ROOT_PASSWORD"
+export MINIO_ROOT_USER="$MINIO_ROOT_USER"
 ./minio server --address :$PORT /mnt/minio/data
