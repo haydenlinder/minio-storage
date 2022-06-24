9898855ff56937b4b85d2ccbf0e05d1fa8276ce5
haydenlinder <haydenlinder@gmail.com>
2022-06-23 17:40:35 -0700

    Add bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 cd /minio
 wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
+rm /bin/sh && ln -s /bin/bash /bin/sh
 export MINIO_ROOT_PASSWORD="$MINIO_ROOT_PASSWORD"
 export MINIO_ROOT_USER="$MINIO_ROOT_USER"
 ./minio server --address :$PORT /mnt/minio/data
