121414f88b7efeef30ad0889631fcb7e3249d9dc
haydenlinder <haydenlinder@gmail.com>
2022-06-23 17:44:05 -0700

    Move script to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM heroku/heroku:20
 
 ADD entrypoint.sh /entrypoint.sh
 
-RUN mkdir -p /mnt/minio/data && \
+RUN rm /bin/sh && ln -s /bin/bash /bin/shn  &&\
+    mkdir -p /mnt/minio/data && \
     chmod +x /entrypoint.sh && \
     mkdir -m 777 /minio
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 cd /minio
 wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
-rm /bin/sh && ln -s /bin/bash /bin/sh
 export MINIO_ROOT_PASSWORD="$MINIO_ROOT_PASSWORD"
 export MINIO_ROOT_USER="$MINIO_ROOT_USER"
 ./minio server --address :$PORT --console-address :9000 /mnt/minio/data
